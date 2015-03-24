8cd57a302a02029765f5ab206542e46a25fbdbfa
oz pozner <oz.pozner@hotmail.com>
2015-03-24 20:11:22 +0200

    fixed initil scaffold errors

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -69,6 +69,6 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.require(:device).permit(:name, :ip_addr, :login, :pass, :nextID, :prevID, :type)
+      params.require(:device).permit(:name, :ip_addr, :login, :pass, :nextID, :prevID, :deviceTypeID)
     end
 end
