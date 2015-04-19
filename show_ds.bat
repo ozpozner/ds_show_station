bfff5300695968f1b82955e51ef5cf4cfa36588e
oz pozner <oz.pozner@hotmail.com>
2015-04-19 10:31:52 -0500

    fixed full screen show

---
+++ show_ds.bat
@@ -0,0 +1,3 @@
+@ping 192.168.1.234 -n 5 >> null
+@nircmd execmd "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --start-fullscreen http://127.0.0.1:3030/map/index
+@nircmd sendkey press f11
